5e483e344e3a2709266358fad6692b9a8239c2bc
Mingchao Lian <lianmccc@Mingchaos-MBP.fios-router.home>
2020-05-23 16:46:54 -0700

    update

--- lab3a.c
+++ lab3a.c
@@ -11,11 +11,10 @@ int device_fd;
 struct ext2_super_block superblock;
 int block_size;
 
+// takes a block number and return its offset 
 long block_offset(int block_num) {
 	return 1024 + (block_num - 1) * block_size;
 }
-
-
 
 int main (int argc, char* argv[]) {
 	if (argc != 2) {
@@ -33,24 +32,23 @@ int main (int argc, char* argv[]) {
 		exit(2);
     }
 
-    // get the superblock size
-    block_size = EXT2_MIN_BLOCK_SIZE << superblock.s_log_block_size;
-
     // superblock summary
 	fprintf(stdout, "SUPERBLOCK,%d,%d,%d,%d,%d,%d,%d\n", 
-    superblock.s_blocks_count,      // total number of blocks (decimal)
-    superblock.s_inodes_count,      // total number of i-nodes (decimal)
-    block_size,	                    // block size (in bytes, decimal)
-    superblock.s_inode_size,        // i-node size (in bytes, decimal)
-    superblock.s_blocks_per_group,  // blocks per group (decimal)
-    superblock.s_inodes_per_group,  // i-nodes per group (decimal)
-    superblock.s_first_ino          // first non-reserved i-node (decimal)
+        superblock.s_blocks_count,      // total number of blocks (decimal)
+        superblock.s_inodes_count,      // total number of i-nodes (decimal)
+        block_size,	                    // block size (in bytes, decimal)
+        superblock.s_inode_size,        // i-node size (in bytes, decimal)
+        superblock.s_blocks_per_group,  // blocks per group (decimal)
+        superblock.s_inodes_per_group,  // i-nodes per group (decimal)
+        superblock.s_first_ino          // first non-reserved i-node (decimal)
 	);
+
+    // get the block size from superblock
+    block_size = EXT2_MIN_BLOCK_SIZE << superblock.s_log_block_size;
 
     
 
-
-
+    
 
     exit(0);
 }
